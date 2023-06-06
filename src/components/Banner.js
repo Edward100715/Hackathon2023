67bac879818774b119ab9468c4195b4cb7bb110f
Edward Berm <edwardbermudez103@gmail.com>
2023-06-05 20:52:52 -0500

    V1.4

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ export const Banner = () => {
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const [index, setIndex] = useState(1);
-    const toRotate = ["Programadores", "Diseñadores", "Comunicadores", "Analistas de datos", "Y Más Perfiles."];
+    const toRotate = ["Programadores", "Diseñadores", "Comunicadores", "Analistas De Datos", "Y Más Perfiles."];
     const period = 2000;
 
     useEffect(() => {
